08993b8a2bedc320300ac904c0ed9ee2af2592a3
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-06-26 00:47:48 +0900

    refactor: Refactor web application configuration into init_app()

--- server.py
+++ server.py
@@ -17,7 +17,8 @@ async def chat_post_handler(request):
 
 
 async def websocket_handler(request):
-    ws = web.WebSocketResponse()
+    app = request.app
+    ws  = web.WebSocketResponse()
     await ws.prepare(request)
 
     app['websockets'].add(ws)
@@ -32,24 +33,26 @@ async def websocket_handler(request):
     return ws
 
 
-# 웹 애플리케이션 관련 설정
-app     = web.Application()                 # aiohttp 웹 애플리케이션 생성 및 할당
-routes  = [
-    web.get('/', index),                    # 사용자에게 보여지는 웹 브라우저
-    web.get('/chat', chat_get_handler),     # Chating 핸들러 (GET)
-    web.post('/chat', chat_post_handler),   # Chating 핸들러 (POST)
-    web.get('/ws', websocket_handler),      # WebSocket 핸들러
-]
-app.add_routes(routes)                      # 웹 애플리케이션 route 등록
-app['websockets'] = set()                   # 웹 소켓 클라이언트 집합 생성
+async def init_app(): # 웹 애플리케이션 관련 설정
+    app     = web.Application()                 # aiohttp 웹 애플리케이션 생성 및 할당
+    routes  = [
+        web.get('/', index),                    # 사용자에게 보여지는 웹 브라우저
+        web.get('/chat', chat_get_handler),     # Chating 핸들러 (GET)
+        web.post('/chat', chat_post_handler),   # Chating 핸들러 (POST)
+        web.get('/ws', websocket_handler),      # WebSocket 핸들러
+    ]
+    app.add_routes(routes)                      # 웹 애플리케이션 route 등록
+    app['websockets'] = set()                   # 웹 소켓 클라이언트 집합 생성
 
 
-# CORS 설정
-cors = aiohttp_cors.setup(app)
-for route in list(app.router.routes()):
-    cors.add(route)
+    # CORS 설정
+    cors = aiohttp_cors.setup(app)
+    for route in list(app.router.routes()):
+        cors.add(route)
+    
+    return app
 
 
 # 서버 실행
 if __name__ == '__main__':
-    web.run_app(app)
+    web.run_app(init_app())
